511f2fd2c8503364fb06594da3225ff43541207e
Felix Niedermann <45990943+FelixNiedermann@users.noreply.github.com>
2026-01-17 11:58:23 +0100

    t

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,9 @@
 from fastapi import FastAPI
 from fastapi.responses import FileResponse
-from backend.fs_api import router
+try:
+    from backend.fs_api import router
+except ModuleNotFoundError:
+    from fs_api import router
 
 app = FastAPI()
 app.include_router(router)
