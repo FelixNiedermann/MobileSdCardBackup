ae0ba3665be64ec328c59b2a8eaa176b26386a2c
Felix Niedermann <45990943+FelixNiedermann@users.noreply.github.com>
2026-01-17 12:09:22 +0100

    trusted middle ware for nginx

--- backend/app.py
+++ backend/app.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 from fastapi import FastAPI
+from fastapi.middleware.trustedhost import TrustedHostMiddleware
 from fastapi.responses import FileResponse
 try:
     from backend.fs_api import router
@@ -8,6 +9,10 @@ except ModuleNotFoundError:
 
 app = FastAPI()
 app.include_router(router)
+app.add_middleware(
+    TrustedHostMiddleware,
+    allowed_hosts=["mobile-backup.local", "127.0.0.1", "localhost"]
+)
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
